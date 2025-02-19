79aeda56bef416ad7a902df071425c8223fd48a8
minhtrannhat <minh@minhtrannhat.com>
2025-02-18 20:10:47 -0500

    feb 2025 update

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "olical/conjure", enabled = false },
   { "PaterJason/cmp-conjure", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
 }
